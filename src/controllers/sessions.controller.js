bfa99e4526bf5778564816716d04e6175b5121f7
Stefan Dios <stefandiosmayarin@gmail.com>
2025-07-11 17:55:29 -0300

    fix: add JWT_SECRET

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -32,13 +32,13 @@ const login = async (req, res) => {
     const isValidPassword = await passwordValidation(user,password);
     if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
     const userDto = UserDTO.getUserTokenFrom(user);
-    const token = jwt.sign(userDto,'tokenSecretJWT',{expiresIn:"1h"});
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
     res.cookie('coderCookie',token,{maxAge:3600000}).send({status:"success",message:"Logged in"})
 }
 
 const current = async(req,res) =>{
     const cookie = req.cookies['coderCookie']
-    const user = jwt.verify(cookie,'tokenSecretJWT');
+    const user = jwt.verify(cookie, process.env.JWT_SECRET);
     if(user)
         return res.send({status:"success",payload:user})
 }
@@ -50,12 +50,12 @@ const unprotectedLogin  = async(req,res) =>{
     if(!user) return res.status(404).send({status:"error",error:"User doesn't exist"});
     const isValidPassword = await passwordValidation(user,password);
     if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
-    const token = jwt.sign(user,'tokenSecretJWT',{expiresIn:"1h"});
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
     res.cookie('unprotectedCookie',token,{maxAge:3600000}).send({status:"success",message:"Unprotected Logged in"})
 }
 const unprotectedCurrent = async(req,res)=>{
     const cookie = req.cookies['unprotectedCookie']
-    const user = jwt.verify(cookie,'tokenSecretJWT');
+    const user = jwt.verify(cookie, process.env.JWT_SECRET);
     if(user)
         return res.send({status:"success",payload:user})
 }
